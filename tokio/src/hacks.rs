d91a8b8186ec7ceaceec15556f8f1a8385261589
Eric Seppanen <eric@matician.com>
2024-06-07 12:08:17 -0700

    add a long poll callback function
    This will allow the caller to decide what to do with the information:
    print it, log it, store it in a list, etc.

--- tokio/src/hacks.rs
+++ tokio/src/hacks.rs
@@ -7,14 +7,23 @@ static HACKS_ENABLED: AtomicBool = AtomicBool::new(false);
 
 // SAFETY: not really.
 static mut LONG_POLL_THRESHOLD: Duration = Duration::MAX;
+static mut LONG_POLL_CALLBACK: LongPollCallback = stub_callback;
+
+type GetTypeNameFn = fn() -> &'static str;
+
+/// A callback function, called when a long poll is detected.
+pub type LongPollCallback = fn(Duration, &'static str);
+
+fn stub_callback(_: Duration, _: &'static str) {}
 
 /// Enable hacks. These may have serious performance implications.
 ///
 /// `long_poll_threshold` sets the time that a `Future::poll()` is allowed to run.
 /// Above this value, we will print a warning.
-pub fn init_hacks(long_poll_threshold: Duration) {
+pub fn init_hacks(long_poll_threshold: Duration, long_poll_callback: LongPollCallback) {
     unsafe {
         LONG_POLL_THRESHOLD = long_poll_threshold;
+        LONG_POLL_CALLBACK = long_poll_callback;
     }
     let prev = HACKS_ENABLED.swap(true, Ordering::Release);
     if prev {
@@ -22,12 +31,11 @@ pub fn init_hacks(long_poll_threshold: Duration) {
     }
 }
 
-type GetTypeNameFn = fn() -> &'static str;
-
 /// Print a warning message if the poll time was too long.
 pub(crate) fn check_long_poll(poll_duration: Duration, type_fn: GetTypeNameFn) {
     if poll_duration > unsafe { LONG_POLL_THRESHOLD } {
         let name = type_fn();
-        eprintln!("long poll {poll_duration:?} {name}");
+        let callback_fn = unsafe { LONG_POLL_CALLBACK };
+        (callback_fn)(poll_duration, name);
     }
 }
